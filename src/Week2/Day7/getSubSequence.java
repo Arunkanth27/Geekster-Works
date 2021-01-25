8da651977c7cb7382f1ec4a73b2085523c79c974
dhanan27599 <dhananjayan.dayalan@gmail.com>
2021-01-25 18:20:57 +0530

    Get Subsequence Update

--- src/Week2/Day7/getSubSequence.java
+++ src/Week2/Day7/getSubSequence.java
@@ -11,21 +11,20 @@ public class getSubSequence {
 
     private static ArrayList<String> solve(String s) {
         if(s.length() == 0) {
-            ArrayList<String> sample = new ArrayList<>();
-            sample.add("");
-            return sample;
+            ArrayList<String> br = new ArrayList<>();
+            br.add("");
+            return br;
         }
 
-        char ch = s.charAt(0);
-        String str = s.substring(1);
-        ArrayList<String> result = solve(str);
-        ArrayList<String> resultnew = new ArrayList<>();
-        for(String st : result) {
-            resultnew.add(st);
-            resultnew.add(ch + st);
+        char firstchar = s.charAt(0);
+        ArrayList<String> recursionresult = solve(s.substring(1));
+        ArrayList<String> finalresult = new ArrayList<>();
+        for(String str : recursionresult) {
+            finalresult.add(str);
+            finalresult.add(firstchar + str);
         }
 
-        return resultnew;
+        return finalresult;
     }
 
 
