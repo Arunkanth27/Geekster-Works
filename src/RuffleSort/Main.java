24052b3352557919055336ef03948d3490c248da
Arunkanth27 <ak524607@gmail.com>
2021-02-15 21:22:07 +0530

    Main

---
+++ src/RuffleSort/Main.java
@@ -0,0 +1,14 @@
+package RuffleSort;
+
+public class Main {
+	
+	public static void main(String[] args)
+	{
+		int [] arr = {1,3,4,2,5};
+		Ruffle.sort(arr);
+		for(int k:arr)
+		{
+		System.out.println(k);
+	}}
+
+}
